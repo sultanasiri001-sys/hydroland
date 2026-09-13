5381781eb3b50976852c25aeaa4e6d9990cb3474
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-13 12:32:42 +0300

    feat(auth): export access token guard

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { AccessTokenGuard } from './access-token.guard';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthService, AccessTokenGuard],
+  exports: [AuthService, AccessTokenGuard],
 })
 export class AuthModule {}
