20db45091533cd18ac5bed7b506dde2dd1c442de
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-13 12:50:07 +0300

    feat(documents): add documents module

---
+++ apps/api/src/documents/documents.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { DocumentsController } from './documents.controller';
+import { DocumentsService } from './documents.service';
+
+@Module({
+  imports: [AuthModule],
+  controllers: [DocumentsController],
+  providers: [DocumentsService],
+  exports: [DocumentsService],
+})
+export class DocumentsModule {}
