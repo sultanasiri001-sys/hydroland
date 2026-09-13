d2e51800860666c53202f97c1cd15b36220854cb
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-13 13:25:12 +0300

    feat(notifications): connect review decisions to notifications

--- apps/api/src/review/review.module.ts
+++ apps/api/src/review/review.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AccessModule } from '../access/access.module';
 import { AuthModule } from '../auth/auth.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { ReviewController } from './review.controller';
 import { ReviewService } from './review.service';
 
 @Module({
-  imports: [AuthModule, AccessModule],
+  imports: [AuthModule, AccessModule, NotificationsModule],
   controllers: [ReviewController],
   providers: [ReviewService],
   exports: [ReviewService],
