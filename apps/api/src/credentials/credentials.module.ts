31704627b5d27c1d864f9f35decaae963d9ec78a
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-15 00:24:48 +0300

    feat(credentials): add audit trail support

--- apps/api/src/credentials/credentials.module.ts
+++ apps/api/src/credentials/credentials.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuditModule } from '../audit/audit.module';
 import { AuthModule } from '../auth/auth.module';
 import { TripsModule } from '../trips/trips.module';
 import { CredentialsController } from './credentials.controller';
 import { CredentialsService } from './credentials.service';
 
-@Module({imports:[AuthModule,TripsModule],controllers:[CredentialsController],providers:[CredentialsService]})
+@Module({imports:[AuthModule,TripsModule,AuditModule],controllers:[CredentialsController],providers:[CredentialsService]})
 export class CredentialsModule {}
