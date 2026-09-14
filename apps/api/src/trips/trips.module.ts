778f215734cb628bf1665e313f16bb14b301e122
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-14 20:48:03 +0300

    feat(api): register governed trip admin routes

--- apps/api/src/trips/trips.module.ts
+++ apps/api/src/trips/trips.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from '../admin/admin.module';
 import { AuthModule } from '../auth/auth.module';
+import { TripAdminController } from './trip-admin.controller';
+import { TripAdminService } from './trip-admin.service';
 import { TripsController } from './trips.controller';
 import { TripsService } from './trips.service';
 
 @Module({
   imports: [AuthModule, AdminModule],
-  controllers: [TripsController],
-  providers: [TripsService],
+  controllers: [TripsController, TripAdminController],
+  providers: [TripsService, TripAdminService],
 })
 export class TripsModule {}
