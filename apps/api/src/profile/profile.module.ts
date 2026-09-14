b11f54f32eac39f46b6e88d339336304625f641a
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-14 23:30:33 +0300

    feat(profile): enable centralized equipment policies

--- apps/api/src/profile/profile.module.ts
+++ apps/api/src/profile/profile.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { TripsModule } from '../trips/trips.module';
 import { DiverMasterProfileController } from './diver-master-profile.controller';
 import { DiverMasterProfileService } from './diver-master-profile.service';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TripsModule],
   controllers: [ProfileController, DiverMasterProfileController],
   providers: [ProfileService, DiverMasterProfileService],
 })
