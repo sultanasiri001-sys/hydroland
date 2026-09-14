99cead40aa7199ebf8d7a70be9e7b4eb2f1d95d9
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-15 02:21:56 +0300

    feat(inventory): register stocktake module

--- apps/api/src/trips/trips.module.ts
+++ apps/api/src/trips/trips.module.ts
@@ -16,6 +16,8 @@ import { EquipmentInspectionController } from './equipment-inspection.controller
 import { EquipmentInspectionService } from './equipment-inspection.service';
 import { EquipmentInventoryController } from './equipment-inventory.controller';
 import { EquipmentInventoryService } from './equipment-inventory.service';
+import { InventoryStocktakeController } from './inventory-stocktake.controller';
+import { InventoryStocktakeService } from './inventory-stocktake.service';
 import { OperationalClearanceScheduler } from './operational-clearance.scheduler';
 import { OperationalClearanceService } from './operational-clearance.service';
 import { PolicyControlController } from './policy-control.controller';
@@ -33,8 +35,8 @@ import { WeatherGateService } from './weather-gate.service';
 
 @Module({
   imports: [AuthModule, AdminModule, AuditModule, NotificationsModule],
-  controllers: [TripsController,TripAdminController,TripCompletionController,WeatherGateController,CalendarAllocationController,CrewAssignmentController,PolicyControlController,BoatComplianceController,TripComplianceController,EquipmentInspectionController,EquipmentInventoryController],
-  providers: [TripsService,TripAdminService,TripCompletionService,WeatherGateService,CalendarAllocationService,CalendarResourceService,CrewAssignmentService,CrewEscalationScheduler,OperationalClearanceService,OperationalClearanceScheduler,BookingParticipantService,PolicyControlService,BoatComplianceService,TripComplianceService,EquipmentInspectionService,EquipmentInventoryService],
-  exports: [PolicyControlService,BoatComplianceService,TripComplianceService,EquipmentInspectionService,EquipmentInventoryService],
+  controllers: [TripsController,TripAdminController,TripCompletionController,WeatherGateController,CalendarAllocationController,CrewAssignmentController,PolicyControlController,BoatComplianceController,TripComplianceController,EquipmentInspectionController,EquipmentInventoryController,InventoryStocktakeController],
+  providers: [TripsService,TripAdminService,TripCompletionService,WeatherGateService,CalendarAllocationService,CalendarResourceService,CrewAssignmentService,CrewEscalationScheduler,OperationalClearanceService,OperationalClearanceScheduler,BookingParticipantService,PolicyControlService,BoatComplianceService,TripComplianceService,EquipmentInspectionService,EquipmentInventoryService,InventoryStocktakeService],
+  exports: [PolicyControlService,BoatComplianceService,TripComplianceService,EquipmentInspectionService,EquipmentInventoryService,InventoryStocktakeService],
 })
 export class TripsModule {}
