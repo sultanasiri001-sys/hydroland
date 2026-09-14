41f7b94916afbdf2a381f7dd27ca3005ba97107d
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-14 21:36:35 +0300

    feat(api): register diver master profile

--- apps/api/src/profile/profile.module.ts
+++ apps/api/src/profile/profile.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { DiverMasterProfileController } from './diver-master-profile.controller';
+import { DiverMasterProfileService } from './diver-master-profile.service';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
-@Module({ imports: [AuthModule], controllers: [ProfileController], providers: [ProfileService] })
+
+@Module({
+  imports: [AuthModule],
+  controllers: [ProfileController, DiverMasterProfileController],
+  providers: [ProfileService, DiverMasterProfileService],
+})
 export class ProfileModule {}
