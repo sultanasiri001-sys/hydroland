018ae0c63a3a9a67f31d6ee24fe7495fcd160e94
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-17 00:55:25 +0300

    feat(finance): register canonical accountant access resolver

--- apps/api/src/finance/finance.module.ts
+++ apps/api/src/finance/finance.module.ts
@@ -1,7 +1,8 @@
 import {Module} from '@nestjs/common';
 import {DatabaseModule} from '../database/database.module';
 import {FinanceController} from './finance.controller';
+import {FinanceAccessService} from './finance-access.service';
 import {FinanceShiftsService} from './finance-shifts.service';
 
-@Module({imports:[DatabaseModule],controllers:[FinanceController],providers:[FinanceShiftsService],exports:[FinanceShiftsService]})
+@Module({imports:[DatabaseModule],controllers:[FinanceController],providers:[FinanceAccessService,FinanceShiftsService],exports:[FinanceAccessService,FinanceShiftsService]})
 export class FinanceModule{}
