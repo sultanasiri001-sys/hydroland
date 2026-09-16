9da6732c6610de2ffa79bf87bdf0e427c6dc3738
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-16 11:20:11 +0300

    feat(governance): expose governance service

--- apps/api/src/governance/governance.module.ts
+++ apps/api/src/governance/governance.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { GovernanceService } from './governance.service';
 import { PolicyEngineService } from './policy-engine.service';
 
 @Module({
-  providers: [PolicyEngineService],
-  exports: [PolicyEngineService],
+  providers: [PolicyEngineService, GovernanceService],
+  exports: [PolicyEngineService, GovernanceService],
 })
 export class GovernanceModule {}
