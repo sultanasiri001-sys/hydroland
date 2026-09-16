b55e571d0e6785b5f05026948d847499398b755d
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-16 10:52:59 +0300

    feat(api): wire governance module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { GovernanceModule } from './governance/governance.module';
 import { HealthController } from './health/health.controller';
 
 @Module({
+  imports: [GovernanceModule],
   controllers: [HealthController],
 })
 export class AppModule {}
