6653d0bc152aa9567de64ada8025cc780e16468f
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-17 01:35:16 +0300

    feat(finance): wire receivables service into finance module

--- apps/api/src/finance/finance.module.ts
+++ apps/api/src/finance/finance.module.ts
@@ -3,6 +3,7 @@ import {DatabaseModule} from '../database/database.module';
 import {FinanceController} from './finance.controller';
 import {FinanceAccessService} from './finance-access.service';
 import {FinanceShiftsService} from './finance-shifts.service';
+import {FinanceReceivablesService} from './finance-receivables.service';
 
-@Module({imports:[DatabaseModule],controllers:[FinanceController],providers:[FinanceAccessService,FinanceShiftsService],exports:[FinanceAccessService,FinanceShiftsService]})
+@Module({imports:[DatabaseModule],controllers:[FinanceController],providers:[FinanceAccessService,FinanceShiftsService,FinanceReceivablesService],exports:[FinanceAccessService,FinanceShiftsService,FinanceReceivablesService]})
 export class FinanceModule{}
