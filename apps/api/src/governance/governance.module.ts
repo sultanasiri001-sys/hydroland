c7af84180ffba98218c9bd74906b2611c058eebe
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-16 11:31:23 +0300

    feat(api): wire governance services and controller

--- apps/api/src/governance/governance.module.ts
+++ apps/api/src/governance/governance.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ApprovalEngineService } from './approval-engine.service';
+import { EligibilityService } from './eligibility.service';
+import { GovernanceController } from './governance.controller';
 import { GovernanceService } from './governance.service';
 import { PolicyEngineService } from './policy-engine.service';
 
 @Module({
-  providers: [PolicyEngineService, GovernanceService],
-  exports: [PolicyEngineService, GovernanceService],
+  controllers: [GovernanceController],
+  providers: [PolicyEngineService, GovernanceService, ApprovalEngineService, EligibilityService],
+  exports: [PolicyEngineService, GovernanceService, ApprovalEngineService, EligibilityService],
 })
 export class GovernanceModule {}
