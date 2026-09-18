ac2a22a6d39e111d95b7c6c452035c38104cfd5e
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-18 07:20:26 +0300

    feat(general-l1): wire session service

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { SessionService } from './session.service';
 
-@Module({ controllers: [AuthController] })
+@Module({ controllers:[AuthController], providers:[SessionService], exports:[SessionService] })
 export class AuthModule {}
