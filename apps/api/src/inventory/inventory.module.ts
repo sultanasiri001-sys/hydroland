8eaa76b7c01ea7a9c0e9e63d1bc80062e963a0b5
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-18 15:39:13 +0300

    feat(inventory-l2): register inventory workflow service

--- apps/api/src/inventory/inventory.module.ts
+++ apps/api/src/inventory/inventory.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { InventoryFoundationService } from './inventory-foundation.service';
+import { InventoryWorkflowService } from './inventory-workflow.service';
 
 @Module({
-  providers: [InventoryFoundationService],
-  exports: [InventoryFoundationService],
+  providers: [InventoryFoundationService, InventoryWorkflowService],
+  exports: [InventoryFoundationService, InventoryWorkflowService],
 })
 export class InventoryModule {}
