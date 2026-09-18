abde3202ffeedfe8e44e71201a7ab592344390d1
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-18 17:32:57 +0300

    feat(administrative-affairs-l3): register operations service

--- apps/api/src/administrative-affairs/administrative-affairs.module.ts
+++ apps/api/src/administrative-affairs/administrative-affairs.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdministrativeAffairsFoundationService } from './administrative-affairs-foundation.service';
 import { AdministrativeAffairsWorkflowService } from './administrative-affairs-workflow.service';
+import { AdministrativeAffairsOperationsService } from './administrative-affairs-operations.service';
 
 @Module({
-  providers: [AdministrativeAffairsFoundationService, AdministrativeAffairsWorkflowService],
-  exports: [AdministrativeAffairsFoundationService, AdministrativeAffairsWorkflowService],
+  providers: [AdministrativeAffairsFoundationService, AdministrativeAffairsWorkflowService, AdministrativeAffairsOperationsService],
+  exports: [AdministrativeAffairsFoundationService, AdministrativeAffairsWorkflowService, AdministrativeAffairsOperationsService],
 })
 export class AdministrativeAffairsModule {}
