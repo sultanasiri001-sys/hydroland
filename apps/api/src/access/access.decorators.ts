2c2fe81742293626ca220c0fc34c757bde0de008
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-18 07:31:22 +0300

    feat(access): add route permission metadata decorators

---
+++ apps/api/src/access/access.decorators.ts
@@ -0,0 +1,7 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const PERMISSION_KEY = 'hydroland:permission';
+export const SCOPE_PARAM_KEY = 'hydroland:scope-param';
+
+export const RequirePermission = (permission: string) => SetMetadata(PERMISSION_KEY, permission);
+export const ResourceScopeParam = (paramName: string) => SetMetadata(SCOPE_PARAM_KEY, paramName);
