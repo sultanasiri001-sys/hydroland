cca0ac20214fdea509d5267b0a5a6c22e61b4d57
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-18 15:54:38 +0300

    feat(inventory-l4): register inventory governance service

--- apps/api/src/inventory/inventory.module.ts
+++ apps/api/src/inventory/inventory.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { InventoryFoundationService } from './inventory-foundation.service';
 import { InventoryWorkflowService } from './inventory-workflow.service';
 import { InventoryOperationsService } from './inventory-operations.service';
+import { InventoryGovernanceService } from './inventory-governance.service';
 
 @Module({
-  providers: [InventoryFoundationService, InventoryWorkflowService, InventoryOperationsService],
-  exports: [InventoryFoundationService, InventoryWorkflowService, InventoryOperationsService],
+  providers: [InventoryFoundationService, InventoryWorkflowService, InventoryOperationsService, InventoryGovernanceService],
+  exports: [InventoryFoundationService, InventoryWorkflowService, InventoryOperationsService, InventoryGovernanceService],
 })
 export class InventoryModule {}
