f6495964e137c7e4c66a4bd97d6d33632f873484
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-18 17:23:34 +0300

    feat(facilities-maintenance-l3): register operations service

--- apps/api/src/facilities-maintenance/facilities-maintenance.module.ts
+++ apps/api/src/facilities-maintenance/facilities-maintenance.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FacilitiesMaintenanceFoundationService } from './facilities-maintenance-foundation.service';
 import { FacilitiesMaintenanceWorkflowService } from './facilities-maintenance-workflow.service';
+import { FacilitiesMaintenanceOperationsService } from './facilities-maintenance-operations.service';
 
 @Module({
-  providers: [FacilitiesMaintenanceFoundationService, FacilitiesMaintenanceWorkflowService],
-  exports: [FacilitiesMaintenanceFoundationService, FacilitiesMaintenanceWorkflowService],
+  providers: [FacilitiesMaintenanceFoundationService, FacilitiesMaintenanceWorkflowService, FacilitiesMaintenanceOperationsService],
+  exports: [FacilitiesMaintenanceFoundationService, FacilitiesMaintenanceWorkflowService, FacilitiesMaintenanceOperationsService],
 })
 export class FacilitiesMaintenanceModule {}
