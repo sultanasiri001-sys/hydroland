88c62b2dd6034bd67b9edcd72ee90ee8b47c2138
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-18 07:21:21 +0300

    feat(general-l1): register access guard

--- apps/api/src/access/access.module.ts
+++ apps/api/src/access/access.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AccessGuard } from './access.guard';
 import { AccessService } from './access.service';
 
-@Module({ providers:[AccessService], exports:[AccessService] })
+@Module({ providers:[AccessService,AccessGuard], exports:[AccessService,AccessGuard] })
 export class AccessModule {}
