18660e65de086e2467d8e683d657ee5057fa320b
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-18 07:32:34 +0300

    feat(workflow): register PostgreSQL unit of work

--- apps/api/src/database/database.module.ts
+++ apps/api/src/database/database.module.ts
@@ -1,5 +1,6 @@
 import { Global, Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { PostgresUnitOfWork } from './postgres-uow.service';
 @Global()
-@Module({providers:[DatabaseService],exports:[DatabaseService]})
+@Module({providers:[DatabaseService,PostgresUnitOfWork],exports:[DatabaseService,PostgresUnitOfWork]})
 export class DatabaseModule {}
