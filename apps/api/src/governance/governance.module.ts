0cd88fd904699cdbe2b70d2545178b9097256968
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-19 00:29:08 +0300

    feat(training): register training controller

--- apps/api/src/governance/governance.module.ts
+++ apps/api/src/governance/governance.module.ts
@@ -7,9 +7,11 @@ import { TrainingWorkflowService } from './training-workflow.service';
 import { TrainingOperationsService } from './training-operations.service';
 import { TrainingGovernanceService } from './training-governance.service';
 import { TrainingRepositoryService } from './training-repository.service';
+import { TrainingController } from './training.controller';
 
 @Module({
   imports: [AuditModule],
+  controllers: [TrainingController],
   providers: [PolicyEngineService, ApprovalEngineService, TrainingEligibilityService, TrainingWorkflowService, TrainingOperationsService, TrainingGovernanceService, TrainingRepositoryService],
   exports: [PolicyEngineService, ApprovalEngineService, TrainingEligibilityService, TrainingWorkflowService, TrainingOperationsService, TrainingGovernanceService, TrainingRepositoryService, AuditModule],
 })
