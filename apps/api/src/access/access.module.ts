d20da8f0340d3b890e465a383ea2b3c2186d45f8
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-18 07:20:14 +0300

    feat(general-l1): implement apps/api/src/access/access.module.ts

---
+++ apps/api/src/access/access.module.ts
@@ -0,0 +1,5 @@
+import { Module } from '@nestjs/common';
+import { AccessService } from './access.service';
+
+@Module({ providers:[AccessService], exports:[AccessService] })
+export class AccessModule {}
