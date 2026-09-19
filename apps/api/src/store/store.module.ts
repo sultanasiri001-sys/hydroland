b6d71784355cf0da5b1f1f3331f318181a778efe
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-19 04:22:50 +0300

    fix(store): import auth module for access token guard

--- apps/api/src/store/store.module.ts
+++ apps/api/src/store/store.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { StoreController } from './store.controller';
 import { StoreService } from './store.service';
-@Module({controllers:[StoreController],providers:[StoreService]})
+@Module({imports:[AuthModule],controllers:[StoreController],providers:[StoreService]})
 export class StoreModule {}
