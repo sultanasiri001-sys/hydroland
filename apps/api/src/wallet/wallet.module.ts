044d7f172565c4de0f381995d2e8122e52768a3c
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-19 08:12:09 +0300

    feat(wallet): add wallet module

---
+++ apps/api/src/wallet/wallet.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { WalletController } from './wallet.controller';
+import { WalletService } from './wallet.service';
+@Module({imports:[AuthModule],controllers:[WalletController],providers:[WalletService],exports:[WalletService]})
+export class WalletModule {}
