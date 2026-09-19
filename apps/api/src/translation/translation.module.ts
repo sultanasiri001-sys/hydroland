446f2679c2d0ffd2791f049781e031e41e0beb48
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-19 16:15:24 +0300

    fix(translation): wire database module

--- apps/api/src/translation/translation.module.ts
+++ apps/api/src/translation/translation.module.ts
@@ -1,5 +1,6 @@
 import {Module} from '@nestjs/common';
+import {DatabaseModule} from '../database/database.module';
 import {TranslationController} from './translation.controller';
 import {TranslationService} from './translation.service';
-@Module({controllers:[TranslationController],providers:[TranslationService],exports:[TranslationService]})
+@Module({imports:[DatabaseModule],controllers:[TranslationController],providers:[TranslationService],exports:[TranslationService]})
 export class TranslationModule{}
