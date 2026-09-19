cf0282372f3e998f7eb894dda6062e361af7314b
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-19 16:25:45 +0300

    feat(translation): register provider router

--- apps/api/src/translation/translation.module.ts
+++ apps/api/src/translation/translation.module.ts
@@ -2,5 +2,6 @@ import {Module} from '@nestjs/common';
 import {DatabaseModule} from '../database/database.module';
 import {TranslationController} from './translation.controller';
 import {TranslationService} from './translation.service';
-@Module({imports:[DatabaseModule],controllers:[TranslationController],providers:[TranslationService],exports:[TranslationService]})
+import {TranslationRouterService} from './translation-router.service';
+@Module({imports:[DatabaseModule],controllers:[TranslationController],providers:[TranslationService,TranslationRouterService],exports:[TranslationService,TranslationRouterService]})
 export class TranslationModule{}
