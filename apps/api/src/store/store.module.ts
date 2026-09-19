1d2573228bc1978ac8f7f57a25f7bacdd2cdd785
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-19 07:10:05 +0300

    feat(store): add protected product administration
    * feat(store): authorize admin product management
    
    * feat(store): add admin product endpoints
    
    * feat(store): add validated product administration

--- apps/api/src/store/store.module.ts
+++ apps/api/src/store/store.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { AdminModule } from '../admin/admin.module';
 import { StoreController } from './store.controller';
 import { StoreService } from './store.service';
-@Module({imports:[AuthModule],controllers:[StoreController],providers:[StoreService]})
+@Module({imports:[AuthModule,AdminModule],controllers:[StoreController],providers:[StoreService]})
 export class StoreModule {}
