8ac9c016685d63a4bdaca437b4fd4c3def53fd86
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-19 08:17:11 +0300

    feat(rewards): add rewards module

---
+++ apps/api/src/rewards/rewards.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { RewardsController } from './rewards.controller';
+import { RewardsService } from './rewards.service';
+@Module({imports:[AuthModule],controllers:[RewardsController],providers:[RewardsService],exports:[RewardsService]})
+export class RewardsModule {}
