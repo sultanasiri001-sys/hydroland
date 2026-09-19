4bf7134f5b4d798a36b0c295e0a41e6daf9dab4c
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-19 15:18:11 +0300

    feat(translation): add translation module

---
+++ apps/api/src/translation/translation.module.ts
@@ -0,0 +1,5 @@
+import {Module} from '@nestjs/common';
+import {TranslationController} from './translation.controller';
+import {TranslationService} from './translation.service';
+@Module({controllers:[TranslationController],providers:[TranslationService],exports:[TranslationService]})
+export class TranslationModule{}
