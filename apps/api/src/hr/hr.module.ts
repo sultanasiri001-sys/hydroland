c4bfa731c7b08899eeecaaafcb24d6d6dcccf49c
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-22 23:00:24 +0300

    feat(hr): register HR lifecycle controller

--- apps/api/src/hr/hr.module.ts
+++ apps/api/src/hr/hr.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { HrService } from './hr.service';
+import { HrController } from './hr.controller';
 
 @Module({
   imports: [DatabaseModule],
+  controllers: [HrController],
   providers: [HrService],
   exports: [HrService],
 })
