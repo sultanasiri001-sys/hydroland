8dab79432800ad0a627c5c0bf2b6f66c9126adf5
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-23 23:08:34 +0300

    feat(documents): register PDF and branding services

--- apps/api/src/document-forms/document-form.module.ts
+++ apps/api/src/document-forms/document-form.module.ts
@@ -6,5 +6,7 @@ import { DocumentPersistenceService } from './document-persistence.service';
 import { DocumentPersistenceController } from './document-persistence.controller';
 import { DocumentAuthorizationService } from './document-authorization.service';
 import { DocumentPrintService } from './document-print.service';
-@Module({imports:[AuthModule],controllers:[DocumentPersistenceController],providers:[DocumentFormService,DocumentLifecycleService,DocumentAuthorizationService,DocumentPersistenceService,DocumentPrintService],exports:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService,DocumentPrintService]})
+import { DocumentPdfService } from './document-pdf.service';
+import { DocumentBrandingService } from './document-branding.service';
+@Module({imports:[AuthModule],controllers:[DocumentPersistenceController],providers:[DocumentFormService,DocumentLifecycleService,DocumentAuthorizationService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService],exports:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService]})
 export class DocumentFormModule {}
