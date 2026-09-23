a9ff1166a104bab8b5f67ef35d8550c95063bc6d
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-24 00:04:21 +0300

    refactor(documents): remove local logo storage provider

--- apps/api/src/document-forms/document-form.module.ts
+++ apps/api/src/document-forms/document-form.module.ts
@@ -8,7 +8,6 @@ import { DocumentAuthorizationService } from './document-authorization.service';
 import { DocumentPrintService } from './document-print.service';
 import { DocumentPdfService } from './document-pdf.service';
 import { DocumentBrandingService } from './document-branding.service';
-import { LocalDocumentAssetStorage } from './document-asset-storage.service';
 import { DocumentAssetService } from './document-asset.service';
-@Module({imports:[AuthModule],controllers:[DocumentPersistenceController],providers:[DocumentFormService,DocumentLifecycleService,DocumentAuthorizationService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService,LocalDocumentAssetStorage,DocumentAssetService],exports:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService,LocalDocumentAssetStorage,DocumentAssetService]})
+@Module({imports:[AuthModule],controllers:[DocumentPersistenceController],providers:[DocumentFormService,DocumentLifecycleService,DocumentAuthorizationService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService,DocumentAssetService],exports:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService,DocumentAssetService]})
 export class DocumentFormModule {}
