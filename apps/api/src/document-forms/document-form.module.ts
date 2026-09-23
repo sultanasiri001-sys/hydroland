edfcf1218b7297053f40185a708ef91d9f5eff8b
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-23 23:23:25 +0300

    feat(documents): register secure document asset services

--- apps/api/src/document-forms/document-form.module.ts
+++ apps/api/src/document-forms/document-form.module.ts
@@ -8,5 +8,7 @@ import { DocumentAuthorizationService } from './document-authorization.service';
 import { DocumentPrintService } from './document-print.service';
 import { DocumentPdfService } from './document-pdf.service';
 import { DocumentBrandingService } from './document-branding.service';
-@Module({imports:[AuthModule],controllers:[DocumentPersistenceController],providers:[DocumentFormService,DocumentLifecycleService,DocumentAuthorizationService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService],exports:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService]})
+import { LocalDocumentAssetStorage } from './document-asset-storage.service';
+import { DocumentAssetService } from './document-asset.service';
+@Module({imports:[AuthModule],controllers:[DocumentPersistenceController],providers:[DocumentFormService,DocumentLifecycleService,DocumentAuthorizationService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService,LocalDocumentAssetStorage,DocumentAssetService],exports:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService,LocalDocumentAssetStorage,DocumentAssetService]})
 export class DocumentFormModule {}
