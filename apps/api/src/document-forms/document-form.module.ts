4479cabf0ded3e96ce689b6cc7ff359ce35084a0
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-23 19:14:29 +0300

    fix(documents): import AuthModule for document route guard

--- apps/api/src/document-forms/document-form.module.ts
+++ apps/api/src/document-forms/document-form.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { DocumentFormService } from './document-form.service';
 import { DocumentLifecycleService } from './document-lifecycle.service';
 import { DocumentPersistenceService } from './document-persistence.service';
 import { DocumentPersistenceController } from './document-persistence.controller';
-@Module({controllers:[DocumentPersistenceController],providers:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService],exports:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService]})
+@Module({imports:[AuthModule],controllers:[DocumentPersistenceController],providers:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService],exports:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService]})
 export class DocumentFormModule {}
