1cbee8a237f8bd1e9d2f3dc806de2dd2d2e11ce7
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-26 02:30:28 +0300

    feat(auth): register MFA service

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MfaService } from './mfa.service';
 import { AccessTokenGuard } from './access-token.guard';
 import { AuditModule } from '../audit/audit.module';
-@Module({ imports:[forwardRef(()=>AuditModule)], controllers: [AuthController], providers: [AuthService, AccessTokenGuard], exports: [AuthService, AccessTokenGuard] })
+@Module({ imports:[forwardRef(()=>AuditModule)], controllers: [AuthController], providers: [AuthService, MfaService, AccessTokenGuard], exports: [AuthService, MfaService, AccessTokenGuard] })
 export class AuthModule {}
