b03364ffddf810fd5ac812d2d93f8b55583c9787
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-25 19:42:19 +0300

    feat(documents): wire organization document list service

--- apps/api/src/document-forms/document-form.module.ts
+++ apps/api/src/document-forms/document-form.module.ts
@@ -5,9 +5,10 @@ import { DocumentLifecycleService } from './document-lifecycle.service';
 import { DocumentPersistenceService } from './document-persistence.service';
 import { DocumentPersistenceController } from './document-persistence.controller';
 import { DocumentAuthorizationService } from './document-authorization.service';
+import { DocumentListService } from './document-list.service';
 import { DocumentPrintService } from './document-print.service';
 import { DocumentPdfService } from './document-pdf.service';
 import { DocumentBrandingService } from './document-branding.service';
 import { DocumentAssetService } from './document-asset.service';
-@Module({imports:[AuthModule],controllers:[DocumentPersistenceController],providers:[DocumentFormService,DocumentLifecycleService,DocumentAuthorizationService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService,DocumentAssetService],exports:[DocumentFormService,DocumentLifecycleService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService,DocumentAssetService]})
+@Module({imports:[AuthModule],controllers:[DocumentPersistenceController],providers:[DocumentFormService,DocumentLifecycleService,DocumentAuthorizationService,DocumentListService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService,DocumentAssetService],exports:[DocumentFormService,DocumentLifecycleService,DocumentListService,DocumentPersistenceService,DocumentPrintService,DocumentPdfService,DocumentBrandingService,DocumentAssetService]})
 export class DocumentFormModule {}
