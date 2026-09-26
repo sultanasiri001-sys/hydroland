8284ccd62e95067955e325bae697f60b63027792
sultanasiri001-sys <sultanasiri001@gmail.com>
2026-09-26 03:44:32 +0300

    feat(email): register auth challenge delivery coordinator

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthChallengeDeliveryService } from './auth-challenge-delivery.service';
 import { MfaService } from './mfa.service';
 import { GoogleIdentityService } from './google-identity.service';
 import { AccessTokenGuard } from './access-token.guard';
 import { AuditModule } from '../audit/audit.module';
 import { IntegrationModule } from '../integrations/integration.module';
-@Module({ imports:[forwardRef(()=>AuditModule),forwardRef(()=>IntegrationModule)], controllers: [AuthController], providers: [AuthService, MfaService, GoogleIdentityService, AccessTokenGuard], exports: [AuthService, MfaService, GoogleIdentityService, AccessTokenGuard] })
+@Module({ imports:[forwardRef(()=>AuditModule),forwardRef(()=>IntegrationModule)], controllers: [AuthController], providers: [AuthService, AuthChallengeDeliveryService, MfaService, GoogleIdentityService, AccessTokenGuard], exports: [AuthService, MfaService, GoogleIdentityService, AccessTokenGuard] })
 export class AuthModule {}
